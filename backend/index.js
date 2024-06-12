1990049640d91546503ab93768b64de660d5127c
kosiiito <136093375+kosiiito@users.noreply.github.com>
2024-06-12 12:50:07 +0300

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
+const Keycloak = require('keycloak-connect');
+const session = require('express-session');
 
 const app = express();
 app.use(cors());
 
+const keycloak = new Keycloak({}, {
+  "realm": process.env.KEYCLOAK_REALM,
+  "auth-server-url": process.env.KEYCLOAK_URL,
+  "ssl-required": "external",
+  "resource": process.env.KEYCLOAK_CLIENT_ID,
+  "credentials": {
+    "secret": process.env.KEYCLOAK_CLIENT_SECRET
+  }
+});
+
+app.use(session({
+  secret: 'some secret',
+  resave: false,
+  saveUninitialized: true,
+  store: new session.MemoryStore()
+}));
+
+app.use(keycloak.middleware());
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: 3307,  
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
@@ -20,7 +42,7 @@ db.connect(err => {
   console.log('Connected to the MariaDB database');
 });
 
-app.get('/', (req, res) => {
+app.get('/', keycloak.protect(), (req, res) => {
   db.query('SELECT * FROM your_table_name', (err, results) => {
     if (err) {
       res.status(500).send(err);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000', {
+      headers: {
+        'Authorization': `Bearer ${localStorage.getItem('access_token')}`
+      }
+    })
+    .then(response => response.json())
+    .then(data => setData(data))
+    .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
     <div>
-      <h1>Hello from React!</h1>
+      <h1>Data from Backend</h1>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
